b76c2b0b7e5fac17d8909e799ae0d0a2d3e554f0
tresorc <tc@circulor.com>
2024-12-03 20:05:28 +0200

    nav skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,25 @@
-import logo from "./assets/img/brand.png";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+import { BrowserRouter, Outlet, Routes } from "react-router";
+import { Route } from "react-router-dom";
+import Landing from "./pages/landing";
+import MainNavigation from "./components/sections/main-navigation";
 
 function App() {
   return (
-    <div className="w-screen h-screen bg-slate-50/10">
-      <div className="flex flex-col items-center justify-center">
-        <Card className="shadow-none">
-          <CardHeader>
-            <CardTitle>Ventura</CardTitle>
-            <CardDescription>Fund the Future!</CardDescription>
-          </CardHeader>
-          <CardContent>
-            <img src={logo} className="App-logo" alt="logo" />
-          </CardContent>
-          <CardFooter>
-            <p>This is the start</p>
-          </CardFooter>
-        </Card>
-      </div>
+    <div className="max-w-full overflow-hidden">
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <MainNavigation>
+                <Outlet />
+              </MainNavigation>
+            }
+          >
+            <Route path="/" element={<Landing />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
