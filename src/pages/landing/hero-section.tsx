7b5b0292dfd1e8b668ff8781ddc02389afbdba6f
Amily Kassim Kadyl <33954234+amilykassim@users.noreply.github.com>
2024-12-08 11:57:37 +0200

    fix typos

--- src/pages/landing/hero-section.tsx
+++ src/pages/landing/hero-section.tsx
@@ -11,7 +11,7 @@ export function HeroSection() {
         </h1>
         <p className="mx-auto mb-8 max-w-2xl text-lg text-gray-600">
         Ventura is your launchpad to success. Dare to dream, and we’ll fund the journey! 
-        Whether you're building the next big thing or looking to back innovation we're turning groundbreaking ideas into reality.
+        Whether you're building the next big thing or looking to back innovation, we're turning groundbreaking ideas into reality.
         </p>
         <Button className="rounded-full px-8 py-6 text-lg">
           Get started
